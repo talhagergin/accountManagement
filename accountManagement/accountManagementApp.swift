1bc2e76e68f803aa62307a788962aa52463df891
Talha Gergin <talhagergin6@gmail.com>
2025-01-15 00:00:11 +0300

    Initial Commit

---
+++ accountManagement/accountManagementApp.swift
@@ -0,0 +1,17 @@
+//
+//  accountManagementApp.swift
+//  accountManagement
+//
+//  Created by Talha Gergin on 15.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct accountManagementApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
